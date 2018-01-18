c91ba011d2712ee26452c310192b808bf4772b61
luis-vf <luisvf@ufl.edu>
2018-01-18 12:40:10 -0500

    testbench comments

--- ip/sim/register_file_tb/register_file_tb.v
+++ ip/sim/register_file_tb/register_file_tb.v
@@ -1,37 +1,35 @@
 /*
-*Daniel Tola
-*01/17/18
 *
-*Description: Testbench 1 for registerFile
+*testbench for the register file
 *Notes:
 *To-Do
 */
 
 module regfile_tb();
 
-`define DELAY(TIME_CLK) #(10*TIME_CLK); //delays one clock cycle to look nice. Change TIME_CLK to change number of cycles to delays
+`define DELAY(TIME_CLK) #(10*TIME_CLK);
 
 /**********
  * Internal Signals
 **********/
-parameter DATA_WIDTH = 32;
-parameter RD_DEPTH = 2;
-parameter REG_DEPTH = 32;
-parameter ADDR_WIDTH = 5;
+parameter DATA_WIDTH = 32;  //word length
+parameter RD_DEPTH = 2; //number of parallel reads
+parameter REG_DEPTH = 32; //depth of the register file
+parameter ADDR_WIDTH = 5; //address width, for the MIPS the operands are addresses, so operand
 
 reg clk = 0;
 reg rst = 0;
-reg wr = 0;
-reg [ADDR_WIDTH*RD_DEPTH-1:0] rr;
-reg [ADDR_WIDTH-1:0] rw;
-reg [DATA_WIDTH-1:0] d;
-wire [DATA_WIDTH*RD_DEPTH-1:0] q;
+reg wr = 0; //write enable
+reg [ADDR_WIDTH*RD_DEPTH-1:0] rr; //reg read address as vectorized array
+reg [ADDR_WIDTH-1:0] rw; //reg write address
+reg [DATA_WIDTH-1:0] d; //input data to be written to reg
+wire [DATA_WIDTH*RD_DEPTH-1:0] q; //output data from reg reads, vectorized array
 
 reg simState = 0;
 
-/*
-*Synchronous Logic
-*/
+/**********
+ * Synchronous Logic
+ **********/
 always begin
 	if(simState != 1)begin
 		`DELAY(1/2)
@@ -41,7 +39,7 @@ end
 
 initial begin
 	$display($time, "- Starting Sim");
-	$monitor("Changes in output: %d",d);
+	$monitor("Changes in output: %h",d);
 	clk = 0;
 	rst = 1;
 	`DELAY(5)
@@ -49,17 +47,17 @@ initial begin
 	rr = 10'b1010101010;
 	d= 32'hdcaf484c;
 	`DELAY(5)
-	rw = 5'b11011;
+	rw = 5'b11011; //this is the address we are writing to in the register file
 	`DELAY(5)
-	wr = 1;
+	wr = 1; //this starts the process to write the data
 	`DELAY(5)
 	wr = 0;
 	`DELAY(5)
-	d = 32'h37373737;
+	d = 32'h37373737; //data that we are writing
 	`DELAY(5)
-	rw = 5'b00100;
+	rw = 5'b00100;  //this is the address we are writing to in the register file
 	`DELAY(5)
-	wr = 1;
+	wr = 1; //this starts the process to write the data
 	`DELAY(5)
 	wr = 0;
 	`DELAY(5)
